0d7387b0e8f362e90d7caab432e7505fdda80a7d
Lucien <lucien.pouget@efrei.net>
2022-10-22 13:47:49 +0200

    updateItemsdescriptions

--- src/Views/DescriptionsItems.vue
+++ src/Views/DescriptionsItems.vue
@@ -1,4 +1,58 @@
 <template>
-    <h1>Items descriptions</h1>
+    <div class="row row-cols-1 row-cols-md-2 g-4">
+      <div class="row g-0">
+        <div class="col-md-4">
+          <img src="skinsImage/image1.png" class="img-fluid rounded-start" alt="img1" height="350" width="350">
+        </div>
+        <div class="col-md-8">
+          <div class="card-body">
+            <h5 class="card-title">AK-47 Asiimov. Puissante et fiable, l'AK-47 est l'un des fusils d'assaut les plus célèbres au monde. </h5>
+            <p class="card-text">type: "AK-47"</p>
+            <P>skin : "Asiimov"</P>
+            <p>state : "Field-Tested" </p>                     
+            <p>steamVolume : 230 </p>                       
+            <p>prices : 50 </p>                     
+                              
+            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+          </div>
+        </div>
+      </div>
+
+      <div class="row g-0">
+        <div class="col-md-4">
+          <img src="skinsImage/image2.png" class="img-fluid rounded-start" alt="img1" height="350" width="350">
+        </div>
+        <div class="col-md-8">
+          <div class="card-body">
+            <h5 class="card-title">AWP Néo-noir. Reconnaissable au son caractéristique qu'il produit, un seul tir suffit. </h5>
+            <p class="card-text">type: "AWP"</p>
+            <p>skin : "Neo-Noir""</p>
+            <p>state : "Field-Tested" </p>                     
+            <p>steamVolume : 40 </p>                       
+            <p>prices : 27 </p>                     
+                              
+            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+          </div>
+        </div>
+      </div>
+
+      <div class="row g-0">
+        <div class="col-md-4">
+          <img src="skinsImage/image1.png" class="img-fluid rounded-start" alt="img1">
+        </div>
+        <div class="col-md-8">
+          <div class="card-body">
+            <h5 class="card-title">AK-47 Asiimov. Puissante et fiable, l'AK-47 est l'un des fusils d'assaut les plus célèbres au monde. </h5>
+            <p class="card-text">type: "AK-47"</p>
+            <P>skin : "Neo-Noir""</P>
+            <p>state : "Field-Tested" </p>                     
+            <p>steamVolume : 230 </p>                       
+            <p>prices : 50 </p>                     
+                              
+            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+          </div>
+        </div>
+      </div>
+    </div>
      
   </template>
