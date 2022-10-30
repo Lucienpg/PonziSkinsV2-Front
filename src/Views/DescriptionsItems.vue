3aa6de8f46d3885d62ccea95076edf4f9880ce7d
Adrien-Db <73785397+Adrien-Db@users.noreply.github.com>
2022-10-31 00:13:38 +0100

    Update DescriptionsItems.vue
    Add all the items

--- src/Views/DescriptionsItems.vue
+++ src/Views/DescriptionsItems.vue
@@ -38,21 +38,257 @@
 
       <div class="row g-0">
         <div class="col-md-4">
-          <img src="skinsImage/image1.png" class="img-fluid rounded-start" alt="img1">
+          <img src="skinsImage/image3.png" class="img-fluid rounded-start" alt="img1">
         </div>
         <div class="col-md-8">
           <div class="card-body">
-            <h5 class="card-title">AK-47 Asiimov. Puissante et fiable, l'AK-47 est l'un des fusils d'assaut les plus célèbres au monde. </h5>
-            <p class="card-text">type: "AK-47"</p>
-            <P>skin : "Neo-Noir""</P>
-            <p>state : "Field-Tested" </p>                     
-            <p>steamVolume : 230 </p>                       
+            <h5 class="card-title">« La fortune sourit aux audacieux », voici la devise de cette arme tristement célèbre qu'est l'AWP </h5>
+            <p class="card-text">type: "AWP"</p>
+            <P>skin : "Wildfire""</P>
+            <p>state : "Minimal-Wear" </p>                     
+            <p>steamVolume : 40 </p>                       
             <p>prices : 50 </p>                     
                               
             <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
           </div>
         </div>
       </div>
+
+      <div class="row g-0">
+        <div class="col-md-4">
+          <img src="skinsImage/image8.png" class="img-fluid rounded-start" alt="img1" height="350" width="350">
+        </div>
+        <div class="col-md-8">
+          <div class="card-body">
+            <h5 class="card-title">Avec sa lame incurvée imitant une griffe de tigre, le karambit a été développé pour le Pencak silat : un art martial d'Asie du Sud. </h5>
+            <p class="card-text">type: "Karambit Knife"</p>
+            <p>skin : "Fade""</p>
+            <p>state : "Factory-New" </p>                     
+            <p>steamVolume : 5 </p>                       
+            <p>prices : 1800 </p>                     
+                              
+            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+          </div>
+        </div>
+      </div>
+
+      <div class="row g-0">
+        <div class="col-md-4">
+          <img src="skinsImage/image13.png" class="img-fluid rounded-start" alt="img1" height="350" width="350">
+        </div>
+        <div class="col-md-8">
+          <div class="card-body">
+            <h5 class="card-title">Ce couteau de survie à lame fixe est conçu pour être utilisé à la fois comme arme et comme outil. </h5>
+            <p class="card-text">type: "Paracord Knife"</p>
+            <p>skin : "BLue Stell"</p>
+            <p>state : "Minimal-Wear-Tested" </p>                     
+            <p>steamVolume : 4 </p>                       
+            <p>prices : 140 </p>                     
+                              
+            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+          </div>
+        </div>
+      </div>
+
+      <div class="row g-0">
+        <div class="col-md-4">
+          <img src="skinsImage/image12.png" class="img-fluid rounded-start" alt="img1" height="350" width="350">
+        </div>
+        <div class="col-md-8">
+          <div class="card-body">
+            <h5 class="card-title">Ce couteau de chasse et de survie pliant est composé d'un manche ergonomique, d'inserts en composite ainsi que d'une lame large et robuste. </h5>
+            <p class="card-text">type: "Nomad Knife"</p>
+            <p>skin : "Slaughter"</p>
+            <p>state : "Minimal-Wear" </p>                     
+            <p>steamVolume : 4 </p>                       
+            <p>prices : 500 </p>                     
+                              
+            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+          </div>
+        </div>
+      </div>
+
+      <div class="row g-0">
+        <div class="col-md-4">
+          <img src="skinsImage/image4.png" class="img-fluid rounded-start" alt="img1" height="350" width="350">
+        </div>
+        <div class="col-md-8">
+          <div class="card-body">
+            <h5 class="card-title">Relativement inchangée dans sa conception depuis la Seconde Guerre mondiale, la baïonnette conserve toujours sa place dans la stratégie militaire moderne. </h5>
+            <p class="card-text">type: "Bayonet"</p>
+            <p>skin : "Lore"</p>
+            <p>state : "Minimal-Wear" </p>                     
+            <p>steamVolume : 4 </p>                       
+            <p>prices : 500 </p>                     
+                              
+            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+          </div>
+        </div>
+      </div>
+
+      <div class="row g-0">
+        <div class="col-md-4">
+          <img src="skinsImage/image10.png" class="img-fluid rounded-start" alt="img1" height="350" width="350">
+        </div>
+        <div class="col-md-8">
+          <div class="card-body">
+            <h5 class="card-title">Il s'agit de la baïonnette M-9. Initialement destinée à être montée sur un fusil, elle est aussi bien adaptée pour les combats rapprochés. </h5>
+            <p class="card-text">type: "M9 Bayonet"</p>
+            <p>skin : "Tiger Tooth"</p>
+            <p>state : "Minimal-Wear" </p>                     
+            <p>steamVolume : 4 </p>                       
+            <p>prices : 800 </p>                     
+                              
+            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+          </div>
+        </div>
+      </div>
+
+      <div class="row g-0">
+        <div class="col-md-4">
+          <img src="skinsImage/image7.png" class="img-fluid rounded-start" alt="img1" height="350" width="350">
+        </div>
+        <div class="col-md-8">
+          <div class="card-body">
+            <h5 class="card-title">Ces mitaines ont été décorées avec des clous en métal et le logo de l'Opération Hydra. </h5>
+            <p class="card-text">type: "Hydra Gloves"</p>
+            <p>skin : "Case Hardened"</p>
+            <p>state : "Minimal-Wear" </p>                     
+            <p>steamVolume : 4 </p>                       
+            <p>prices : 150 </p>                     
+                              
+            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+          </div>
+        </div>
+      </div>
+
+      <div class="row g-0">
+        <div class="col-md-4">
+          <img src="skinsImage/image14.png" class="img-fluid rounded-start" alt="img1" height="350" width="350">
+        </div>
+        <div class="col-md-8">
+          <div class="card-body">
+            <h5 class="card-title">Les tissus synthétiques utilisés pour ces gants les rendent durables et tape-à-l’œil.</h5>
+            <p class="card-text">type: "Sport Gloves"</p>
+            <p>skin : "Amphibious"</p>
+            <p>state : "Battle-Scarred" </p>                     
+            <p>steamVolume : 4 </p>                       
+            <p>prices : 500 </p>                     
+                              
+            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+          </div>
+        </div>
+      </div>
+
+      <div class="row g-0">
+        <div class="col-md-4">
+          <img src="skinsImage/image6.png" class="img-fluid rounded-start" alt="img1" height="350" width="350">
+        </div>
+        <div class="col-md-8">
+          <div class="card-body">
+            <h5 class="card-title">Le Glock 18 est un bon pistolet de première manche, très efficace contre des adversaires sans protection et qui peut tirer des rafales de trois coups.</h5>
+            <p class="card-text">type: "Glock-18"</p>
+            <p>skin : "Fade"</p>
+            <p>state : "Factory New" </p>                     
+            <p>steamVolume : 4 </p>                       
+            <p>prices : 1100 </p>                     
+                              
+            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+          </div>
+        </div>
+      </div>
+
+      <div class="row g-0">
+        <div class="col-md-4">
+          <img src="skinsImage/image9.png" class="img-fluid rounded-start" alt="img1" height="350" width="350">
+        </div>
+        <div class="col-md-8">
+          <div class="card-body">
+            <h5 class="card-title">Plus précise mais moins puissante que l'AK-47, la M4A4 est le fusil d'assaut automatique de prédilection des antiterroristes.</h5>
+            <p class="card-text">type: "M4A4"</p>
+            <p>skin : "The Emperor"</p>
+            <p>state : "Minimal Wear" </p>                     
+            <p>steamVolume : 40 </p>                       
+            <p>prices : 30 </p>                     
+                              
+            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+          </div>
+        </div>
+      </div>
+
+      <div class="row g-0">
+        <div class="col-md-4">
+          <img src="skinsImage/image16.png" class="img-fluid rounded-start" alt="img1" height="350" width="350">
+        </div>
+        <div class="col-md-8">
+          <div class="card-body">
+            <h5 class="card-title">Un des favoris de Counter-Strike: Source, le pistolet USP possède un silencieux amovible qui diminue le recul tout en réduisant le bruit afin de gagner en discrétion.</h5>
+            <p class="card-text">type: "USP-S"</p>
+            <p>skin : "Kill Confirmed"</p>
+            <p>state : "Minimal Wear" </p>                     
+            <p>steamVolume : 40 </p>                       
+            <p>prices : 75 </p>                     
+                              
+            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+          </div>
+        </div>
+      </div>
+
+      <div class="row g-0">
+        <div class="col-md-4">
+          <img src="skinsImage/image15.png" class="img-fluid rounded-start" alt="img1" height="350" width="350">
+        </div>
+        <div class="col-md-8">
+          <div class="card-body">
+            <h5 class="card-title">Ce karambit au pommeau d'ivoire dispose de rivets en laiton et de fines dents de scie.</h5>
+            <p class="card-text">type: "Talon Knife"</p>
+            <p>skin : "Rust Coat"</p>
+            <p>state : "Minimal Wear" </p>                     
+            <p>steamVolume : 40 </p>                       
+            <p>prices : 300 </p>                     
+                              
+            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+          </div>
+        </div>
+      </div>
+
+      <div class="row g-0">
+        <div class="col-md-4">
+          <img src="skinsImage/image5.png" class="img-fluid rounded-start" alt="img1" height="350" width="350">
+        </div>
+        <div class="col-md-8">
+          <div class="card-body">
+            <h5 class="card-title">Les couteaux pliants ont une lame style persan avec une pointe acérée. Alors que la pointe peut être fragile, la conception globale de ce type de couteau est étonnamment très solide. </h5>
+            <p class="card-text">type: "Flip Knife"</p>
+            <p>skin : "Fade"</p>
+            <p>state : "Minimal Wear" </p>                     
+            <p>steamVolume : 40 </p>                       
+            <p>prices : 500 </p>                     
+                              
+            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+          </div>
+        </div>
+      </div>
+
+      <div class="row g-0">
+        <div class="col-md-4">
+          <img src="skinsImage/image11.png" class="img-fluid rounded-start" alt="img1" height="350" width="350">
+        </div>
+        <div class="col-md-8">
+          <div class="card-body">
+            <h5 class="card-title">Avec sa courte silhouette parallélépipédique, le pistolet mitrailleur MAC-10 se targue d'une cadence de tir élevée mais dispose en contrepartie d'une faible précision et d'un puissant recul.</h5>
+            <p class="card-text">type: "MAC-10"</p>
+            <p>skin : "Stalker"</p>
+            <p>state : "Well-Worn" </p>                     
+            <p>steamVolume : 50 </p>                       
+            <p>prices : 9 </p>                     
+                              
+            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+          </div>
+        </div>
+      </div>
+
+
     </div>
      
   </template>
